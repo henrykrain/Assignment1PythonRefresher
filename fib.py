2d604482c8e89417beb39dfd7504bdb3847fe22e
henrykrain <hkrain@uiowa.edu>
2024-01-30 19:55:23 -0600

    fib works, tried to do matplotlib, it did not work

--- fib.py
+++ fib.py
@@ -1,5 +1,15 @@
 from functools import lru_cache
 import time 
+import matplotlib.pyplot as plt
+def arrs(fib_load_time):
+    arr1 = []
+    for n in range(101):
+        arr1.append(n)
+
+    arr2=[]
+    for n in range(101):
+        arr2.append(fib_load_time)
+    return (plt.plot[arr1][arr2])
 
 def timer(func):
     def wrapper(*args, **kwargs):
@@ -8,6 +18,7 @@ def timer(func):
         result = func(*args, **kwargs)
         end_time = time.time()
         total_time = end_time - begin_time
+        #arrs(total_time)
         print(f"Finished in {total_time}: f{num} -> {result}")
         return result
     return wrapper
@@ -20,5 +31,7 @@ def fib(n:int) -> int:
     return fib(n-1) + fib(n-2)
 
 if __name__ == "__main__":
+    #arrs(fib)
     fib(100)
+    #plt.show()
     
